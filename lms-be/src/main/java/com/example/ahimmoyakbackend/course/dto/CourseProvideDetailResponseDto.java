6a90fd7472866b0ab8485ea2a7311660c45e3115
Tiktokyi <tiktokyi@gmail.com>
2024-11-08 00:33:32 +0900

    add : CourseProvideDetailResponseDto 생성

--- lms-be/src/main/java/com/example/ahimmoyakbackend/course/dto/CourseProvideDetailResponseDto.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/course/dto/CourseProvideDetailResponseDto.java
@@ -1,4 +1,31 @@
 package com.example.ahimmoyakbackend.course.dto;
 
-public record CourseProvideDetailResponseDto() {
+import com.example.ahimmoyakbackend.course.common.CourseProvideState;
+import com.example.ahimmoyakbackend.course.entity.CourseProvide;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record CourseProvideDetailResponseDto(
+        LocalDate beginDate,
+        LocalDate endDate,
+        long attendeeCount,
+        CourseProvideState state,
+        List<EnrollmentInfoDto> learnerList
+) {
+    public static CourseProvideDetailResponseDto from(CourseProvide courseProvide) {
+        return CourseProvideDetailResponseDto.builder()
+                .beginDate(courseProvide.getBeginDate())
+                .endDate(courseProvide.getEndDate())
+                .attendeeCount(courseProvide.getAttendeeCount())
+                .state(courseProvide.getState())
+                .learnerList(
+                        courseProvide.getEnrollments().stream()
+                                .map(EnrollmentInfoDto::from)
+                                .toList()
+                )
+                .build();
+    }
 }
