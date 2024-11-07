a4d6cf67dec72efc963e253965b6ec70f3d8ef4f
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-11-07 16:01:29 +0900

    mod : ContentsController에 updateContent 메소드 생성

--- lms-be/src/main/java/com/example/ahimmoyakbackend/course/controller/ContentsController.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/course/controller/ContentsController.java
@@ -1,9 +1,11 @@
 package com.example.ahimmoyakbackend.course.controller;
 
+import com.example.ahimmoyakbackend.course.dto.ContentUpdateRequestDto;
 import com.example.ahimmoyakbackend.course.dto.ContentsCreateRequestDto;
 import com.example.ahimmoyakbackend.course.dto.ContentsInfoResponseDto;
 import com.example.ahimmoyakbackend.course.service.ContentsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,5 +31,17 @@ public class ContentsController {
     public ResponseEntity<ContentsInfoResponseDto> getContentsInfo(@PathVariable("contentsId") long contentsId) {
         return ResponseEntity.ok(contentsService.getInfo(contentsId));
     }
+
+    public ResponseEntity<String> updateContent(@AuthenticationPrincipal UserDetails userDetails,
+                                                @PathVariable("curriculumId") Long curriculumId,
+                                                @ModelAttribute ContentUpdateRequestDto requestDto
+    ){
+        String target = contentsService.Update(userDetails, curriculumId, requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(target);
+
+    }
+
+
+
 }
 
