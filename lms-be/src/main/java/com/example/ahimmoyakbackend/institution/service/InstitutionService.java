5b0a6a99973d07b38b62203568dc0dce6ff77e42
gomar <ju79318@naver.com>
2024-11-07 15:58:11 +0900

    feat : 교육기관 생성, detail 가져오기, 수정하기, 교육기관id 가져오기 추가

--- lms-be/src/main/java/com/example/ahimmoyakbackend/institution/service/InstitutionService.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/institution/service/InstitutionService.java
@@ -1,7 +1,13 @@
 package com.example.ahimmoyakbackend.institution.service;
 
-import org.springframework.stereotype.Service;
+import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
+import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionRequestDto;
+import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionResponseDto;
+import com.example.ahimmoyakbackend.institution.dto.UserInstitutionIdResponseDto;
 
-@Service
-public class InstitutionService {
+public interface InstitutionService {
+    boolean createInstitution(UserDetailsImpl userDetails, CreateInstitutionRequestDto requestDto);
+    boolean updateInstitution(UserDetailsImpl userDetails, CreateInstitutionRequestDto requestDto);
+    CreateInstitutionResponseDto getInstitutionDetail(UserDetailsImpl userDetails);
+    UserInstitutionIdResponseDto getInstitutionId(UserDetailsImpl userDetails);
 }
