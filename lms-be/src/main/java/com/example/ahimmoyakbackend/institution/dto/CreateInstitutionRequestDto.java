700160072336ca3314c2ade4b41f2f7b17af569b
gomar <ju79318@naver.com>
2024-11-07 15:56:46 +0900

    feat : dto 생성

--- lms-be/src/main/java/com/example/ahimmoyakbackend/institution/dto/CreateInstitutionRequestDto.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/institution/dto/CreateInstitutionRequestDto.java
@@ -1,4 +1,22 @@
 package com.example.ahimmoyakbackend.institution.dto;
 
-public class CreateInstitutionRequestDto {
+import com.example.ahimmoyakbackend.institution.entity.Institution;
+import com.example.ahimmoyakbackend.institution.entity.Manager;
+import lombok.Builder;
+
+@Builder
+public record CreateInstitutionRequestDto(
+    String name,
+    String ownerName,
+    String businessNumber,
+    String certifiedNumber,
+    String email,
+    String phone,
+    Manager manager
+){
+    public static CreateInstitutionRequestDto from(Institution institution){
+        return CreateInstitutionRequestDto.builder()
+                .build();
+    }
+
 }
