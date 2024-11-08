ac21fe7259178a5bb361c5ec262a37dffb532960
Dowon <tnstjs1919@gmail.com>
2024-11-08 10:04:46 +0900

    mod : searchCompany 메서드 responseDto 구현

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/service/CompanyService.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/service/CompanyService.java
@@ -2,13 +2,14 @@ package com.example.ahimmoyakbackend.company.service;
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
 import com.example.ahimmoyakbackend.company.dto.CreateCompanyRequestDto;
+import com.example.ahimmoyakbackend.company.dto.SearchCompanyResponseDto;
 import com.example.ahimmoyakbackend.company.dto.SearchCompanyRequestDto;
 import com.example.ahimmoyakbackend.company.dto.UpdateCompanyRequestDto;
 import com.example.ahimmoyakbackend.global.dto.MessageResponseDto;
 
 public interface CompanyService {
-    public MessageResponseDto createCompany(UserDetailsImpl userDetails, CreateCompanyRequestDto createCompanyRequestDto);
-    public MessageResponseDto searchCompany(UserDetailsImpl userDetails, SearchCompanyRequestDto company);
-    public MessageResponseDto updateCompany(UserDetailsImpl userDetails, Long companyId, UpdateCompanyRequestDto requestDto);
-    public MessageResponseDto deleteCompany(UserDetailsImpl userDetails, Long companyId);
+    MessageResponseDto createCompany(UserDetailsImpl userDetails, CreateCompanyRequestDto createCompanyRequestDto);
+    SearchCompanyResponseDto searchCompany(UserDetailsImpl userDetails, SearchCompanyRequestDto company);
+    MessageResponseDto updateCompany(UserDetailsImpl userDetails, Long companyId, UpdateCompanyRequestDto requestDto);
+    MessageResponseDto deleteCompany(UserDetailsImpl userDetails, Long companyId);
 }
