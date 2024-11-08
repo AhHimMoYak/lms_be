2d5b2f53a830da521d1cafeec348c5659c3733ca
Dowon <tnstjs1919@gmail.com>
2024-11-08 10:05:31 +0900

    feat : 회사생성 requestDto 생성

---
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/dto/CreateCompanyRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.ahimmoyakbackend.company.dto;
+
+import com.example.ahimmoyakbackend.company.entity.Company;
+
+public record CreateCompanyRequestDto(
+        Long id,
+        String name,
+        String owner_name,
+        String business_number,
+        String email,
+        String phone
+) {
+    public Company toEntity() {
+        return Company.builder()
+                .id(id)
+                .name(name)
+                .ownerName(owner_name)
+                .businessNumber(business_number)
+                .email(email)
+                .phone(phone)
+                .build();
+    }
+
+}
