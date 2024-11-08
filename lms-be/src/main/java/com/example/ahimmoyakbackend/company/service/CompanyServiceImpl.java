88ff8093f6448e55041a58b8559ac4a0daabb6c8
Dowon <tnstjs1919@gmail.com>
2024-11-08 10:04:51 +0900

    mod : searchCompany 메서드 responseDto 구현

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/service/CompanyServiceImpl.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/service/CompanyServiceImpl.java
@@ -2,24 +2,27 @@ package com.example.ahimmoyakbackend.company.service;
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
 import com.example.ahimmoyakbackend.company.dto.CreateCompanyRequestDto;
+import com.example.ahimmoyakbackend.company.dto.SearchCompanyResponseDto;
 import com.example.ahimmoyakbackend.company.dto.SearchCompanyRequestDto;
 import com.example.ahimmoyakbackend.company.dto.UpdateCompanyRequestDto;
 import com.example.ahimmoyakbackend.global.dto.MessageResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
 
     @Override
+    @Transactional
     public MessageResponseDto createCompany(UserDetailsImpl userDetails, CreateCompanyRequestDto requestDto) {
         return null;
     }
 
     @Override
-    public MessageResponseDto searchCompany(UserDetailsImpl userDetails, SearchCompanyRequestDto company) {
-        return null;
+    public SearchCompanyResponseDto searchCompany(UserDetailsImpl userDetails, SearchCompanyRequestDto company) {
+        return SearchCompanyResponseDto.builder().build();
     }
 
     @Override
