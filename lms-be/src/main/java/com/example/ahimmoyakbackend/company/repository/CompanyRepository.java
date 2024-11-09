1e9244ae49be81e046787d9b0c5c661bd73bcf24
Dowon <tnstjs1919@gmail.com>
2024-11-09 22:33:33 +0900

    feat : 사업자번호 존재여부 메서드 추가

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
@@ -4,4 +4,6 @@ import com.example.ahimmoyakbackend.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    boolean existsByBusinessNumber(String businessNumber);
 }
