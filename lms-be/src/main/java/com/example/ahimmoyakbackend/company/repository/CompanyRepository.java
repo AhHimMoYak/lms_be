834f0f4ab5474547165d34ca65c54b68471c324e
Dowon <tnstjs1919@gmail.com>
2024-11-09 23:12:55 +0900

    feat : CompanyRepository 에 회사이름 포함되는 리스트 찾는 메서드 생성

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
@@ -3,7 +3,11 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     boolean existsByBusinessNumber(String businessNumber);
+
+    List<Company> findByNameContaining(String name);
 }
