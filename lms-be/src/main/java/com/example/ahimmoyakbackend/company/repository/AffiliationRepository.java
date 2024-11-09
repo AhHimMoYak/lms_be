710e39495ed70741501d7f64279674a5c45c4e19
Dowon <tnstjs1919@gmail.com>
2024-11-10 00:38:54 +0900

    feat : AffiliationRepository 에 userId 로 찾는 메서드 추가

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -3,6 +3,9 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Affiliation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
 
+    Optional<Affiliation> findByUserId(Long userId);
 }
