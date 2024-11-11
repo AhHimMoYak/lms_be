96b8cd06f0a8c45027168d602bc443c92a7862cb
Dowon <tnstjs1919@gmail.com>
2024-11-11 15:20:01 +0900

    feat : AffiliationRepository 에 companyId 찾는 메서드 추가

--- lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -3,9 +3,13 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Affiliation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
 
     Optional<Affiliation> findByUserId(Long userId);
+
+    List<Affiliation> findByCompany_Id(Long id);
+
 }
