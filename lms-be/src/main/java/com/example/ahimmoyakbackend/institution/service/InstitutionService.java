54fd16059d8b9c67c50e97c9dca1809743899523
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-11-11 11:03:25 +0900

    mod : InstitutionService 에 메소드 수정

--- lms-be/src/main/java/com/example/ahimmoyakbackend/institution/service/InstitutionService.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/institution/service/InstitutionService.java
@@ -2,13 +2,14 @@ package com.example.ahimmoyakbackend.institution.service;
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
 import com.example.ahimmoyakbackend.global.dto.MessageResponseDto;
+import com.example.ahimmoyakbackend.institution.dto.UpdateInstitutionRequestDto;
 import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionRequestDto;
-import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionResponseDto;
 import com.example.ahimmoyakbackend.institution.dto.UserInstitutionIdResponseDto;
+import com.example.ahimmoyakbackend.institution.dto.GetInstitutionDetailRequestDto;
 
 public interface InstitutionService {
     MessageResponseDto createInstitution(UserDetailsImpl userDetails, CreateInstitutionRequestDto requestDto);
-    MessageResponseDto updateInstitution(UserDetailsImpl userDetails, CreateInstitutionRequestDto requestDto);
-    CreateInstitutionResponseDto getInstitutionDetail(UserDetailsImpl userDetails);
+    MessageResponseDto updateInstitution(UserDetailsImpl userDetails, UpdateInstitutionRequestDto requestDto, Long institutionId);
+    GetInstitutionDetailRequestDto getInstitutionDetail(UserDetailsImpl userDetails, Long institutionId);
     UserInstitutionIdResponseDto getInstitutionId(UserDetailsImpl userDetails);
 }
