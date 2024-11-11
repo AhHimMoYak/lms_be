4a9e126d65be283c4ce56a0ac62bec475dd12d04
Dowon <tnstjs1919@gmail.com>
2024-11-11 14:45:02 +0900

    feat : EnrollmentRepository 에 id의 enrollment 가 존재하는지 확인하는 메서드 생성

--- lms-be/src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    @Override
+    boolean existsById(Long id);
 
 //    Optional<Enrollment> findByUserAndCourse(User user, Course course);
 
