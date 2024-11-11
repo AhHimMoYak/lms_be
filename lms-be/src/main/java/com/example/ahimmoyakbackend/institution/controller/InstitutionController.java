8038585d0d14660c292ed279fd9ef16adaea66ab
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-11-11 11:02:58 +0900

    mod : createInstitution, getInstitutionDetail, updateInstitution, getInstitutionId 메소드 수정

--- lms-be/src/main/java/com/example/ahimmoyakbackend/institution/controller/InstitutionController.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/institution/controller/InstitutionController.java
@@ -1,9 +1,11 @@
 package com.example.ahimmoyakbackend.institution.controller;
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
+import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.global.dto.MessageResponseDto;
+import com.example.ahimmoyakbackend.institution.dto.GetInstitutionDetailRequestDto;
+import com.example.ahimmoyakbackend.institution.dto.UpdateInstitutionRequestDto;
 import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionRequestDto;
-import com.example.ahimmoyakbackend.institution.dto.CreateInstitutionResponseDto;
 import com.example.ahimmoyakbackend.institution.dto.UserInstitutionIdResponseDto;
 import com.example.ahimmoyakbackend.institution.service.InstitutionService;
 import lombok.RequiredArgsConstructor;
@@ -27,20 +29,28 @@ public class InstitutionController {
     }
 
     @GetMapping()
-    public ResponseEntity<CreateInstitutionResponseDto> getInstitutionDetail(
-            @AuthenticationPrincipal UserDetailsImpl userDetails
+    public ResponseEntity<GetInstitutionDetailRequestDto> getInstitutionDetail(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam Long institutionId
     ){
-        return ResponseEntity.ok(institutionServiceImpl.getInstitutionDetail(userDetails));
+        return ResponseEntity.ok(institutionServiceImpl.getInstitutionDetail(userDetails, institutionId));
     }
 
     @PatchMapping()
     public ResponseEntity<MessageResponseDto> updateInstitution(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateInstitutionRequestDto requestDto
+            @RequestBody UpdateInstitutionRequestDto requestDto,
+            @RequestParam Long institutionId
     ){
-        return ResponseEntity.ok(institutionServiceImpl.createInstitution(userDetails, requestDto));
+        return ResponseEntity.ok(institutionServiceImpl.updateInstitution(userDetails, requestDto, institutionId));
     }
 
+    @GetMapping()
+    public ResponseEntity<UserInstitutionIdResponseDto> getInstitutionId(
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+            ){
+        return ResponseEntity.ok(institutionServiceImpl.getInstitutionId(userDetails));
 
+    }
 
 }
