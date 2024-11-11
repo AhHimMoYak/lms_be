aa3312ab3c38bcf8f33578a4767319d7c60083e5
Dowon <tnstjs1919@gmail.com>
2024-11-11 16:02:57 +0900

    add : CourseProvideList Dto 생성

---
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/company/dto/CourseProvideListDto.java
@@ -0,0 +1,38 @@
+package com.example.ahimmoyakbackend.company.dto;
+
+import com.example.ahimmoyakbackend.company.entity.Company;
+import com.example.ahimmoyakbackend.course.common.CourseProvideState;
+import com.example.ahimmoyakbackend.course.dto.CourseProvideDto;
+import com.example.ahimmoyakbackend.course.entity.Course;
+import com.example.ahimmoyakbackend.course.entity.CourseProvide;
+import com.example.ahimmoyakbackend.institution.entity.Institution;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public record CourseProvideListDto(
+        Long courseProvideId,
+        String courseTitle,
+        String companyName,
+        String institutionName,
+        LocalDate beginDate,
+        LocalDate endDate,
+        CourseProvideState state,
+        long attendeeCount,
+        long deposit
+) {
+    public static CourseProvideDto from(CourseProvide courseProvide, Course course, Company company, Institution institution) {
+        return CourseProvideDto.builder()
+                .courseProvideId(courseProvide.getId())
+                .courseTitle(course.getTitle())
+                .companyName(company.getName())
+                .institutionName(institution.getName())
+                .beginDate(courseProvide.getBeginDate())
+                .endDate(courseProvide.getEndDate())
+                .state(courseProvide.getState())
+                .attendeeCount(courseProvide.getAttendeeCount())
+                .deposit(courseProvide.getDeposit())
+                .build();
+    }
+}
