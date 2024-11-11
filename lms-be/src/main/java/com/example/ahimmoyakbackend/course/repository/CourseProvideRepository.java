aa5fea8ae25227db6969c5c049df8fc85a117505
Dowon <tnstjs1919@gmail.com>
2024-11-11 18:07:33 +0900

    feat : CourseProvideRepository 에 회사 아이디 찾는 메서드 구현

--- lms-be/src/main/java/com/example/ahimmoyakbackend/course/repository/CourseProvideRepository.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/course/repository/CourseProvideRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.course.repository;
 import com.example.ahimmoyakbackend.course.entity.CourseProvide;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseProvideRepository extends JpaRepository<CourseProvide, Long> {
+    List<CourseProvide> findByCompany_Id(Long id);
 }
