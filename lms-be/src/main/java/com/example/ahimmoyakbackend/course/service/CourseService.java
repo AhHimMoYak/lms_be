bd7b535d8da41e62c2f6e707a608e41f20add549
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-11-19 13:50:01 +0900

    mod : userName 받는 메소드추가

--- lms-be/src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
+++ lms-be/src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
@@ -1,10 +1,9 @@
 package com.example.ahimmoyakbackend.course.service;
 
-import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.course.common.CourseCategory;
+import com.example.ahimmoyakbackend.course.dto.CourseCreateRequestDto;
 import com.example.ahimmoyakbackend.course.dto.CourseDetailResponseDto;
 import com.example.ahimmoyakbackend.course.dto.CourseListResponseDto;
-import com.example.ahimmoyakbackend.course.dto.CourseCreateRequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,13 +11,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.List;
 
 public interface CourseService {
-    public CourseDetailResponseDto getDetail(long id);
-    public Long create(UserDetails userDetails, CourseCreateRequestDto requestDto);
-    public boolean update(UserDetails userDetails, long id, CourseCreateRequestDto requestDto);
-    public boolean delete(UserDetails userDetails, long id);
-    public List<CourseListResponseDto> getList(UserDetails userDetails);
-    public List<CourseListResponseDto> getAllList();
-    public Page<CourseListResponseDto> getAllList(Pageable pageable);
-    public List<CourseListResponseDto> getAllList(CourseCategory category);
-    public Page<CourseListResponseDto> getAllList(Pageable pageable, CourseCategory category);
+     CourseDetailResponseDto getDetail(long id);
+     Long create(UserDetails userDetails, CourseCreateRequestDto requestDto);
+     boolean update(UserDetails userDetails, long id, CourseCreateRequestDto requestDto);
+     boolean delete(UserDetails userDetails, long id);
+     List<CourseListResponseDto> getListByInstitution(UserDetails userDetails);
+     List<CourseListResponseDto> getAllList();
+     Page<CourseListResponseDto> getAllList(Pageable pageable);
+     List<CourseListResponseDto> getAllList(CourseCategory category);
+     Page<CourseListResponseDto> getAllList(Pageable pageable, CourseCategory category);
+     List<CourseListResponseDto> getAllList(String userName);
 }
