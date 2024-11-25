b4166f9df331b476088f5d1082d34e3905d65d67
gomar <ju79318@naver.com>
2024-11-25 09:06:42 +0900

    mod : 게시글 삭제될 때 댓글도 같이 삭제 되게 수정ㅇ

--- Board/lambda/board/deleteBoard.mjs
+++ Board/lambda/board/deleteBoard.mjs
@@ -1,15 +1,36 @@
-import {DeleteCommand, DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
-import {docClient} from "../aws-clients.mjs";
+import { DeleteCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { docClient } from "../aws-clients.mjs";
 
 export const handler = async (event) => {
     try {
-        const { id } = event.pathParameters;
+        const { boardId } = event.pathParameters;
 
-        // DynamoDB Delete 명령 실행
+        // 게시글 삭제
         await docClient.send(new DeleteCommand({
             TableName: process.env.BOARD_TABLE,
-            Key: { id }
+            Key: {
+                id: boardId
+            }
         }));
+
+        const commentsQueryResult = await docClient.send(new QueryCommand({
+            TableName: process.env.COMMENT_TABLE,
+            IndexName: 'BoardIdIndex',
+            KeyConditionExpression: 'boardId = :id',
+            ExpressionAttributeValues: {
+                ':id': boardId
+            },
+        }));
+
+        if (commentsQueryResult.Items && commentsQueryResult.Items.length > 0) {
+            const deletePromises = commentsQueryResult.Items.map(comment =>
+                docClient.send(new DeleteCommand({
+                    TableName: process.env.COMMENT_TABLE,
+                    Key: { id: comment.id }
+                }))
+            );
+            await Promise.all(deletePromises);
+        }
 
         return {
             statusCode: 204,
@@ -28,4 +49,4 @@ export const handler = async (event) => {
             body: JSON.stringify({ message: '게시글 삭제에 실패했습니다.' })
         };
     }
-}
+};
