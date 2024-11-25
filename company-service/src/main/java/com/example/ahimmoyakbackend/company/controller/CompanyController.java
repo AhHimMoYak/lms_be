d5cc59843a4380b263c05e7d034bdcb47a29e350
Dowon <tnstjs1919@gmail.com>
2024-11-25 13:50:41 +0900

    mod : company cicd test 3

--- company-service/src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
+++ company-service/src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
@@ -21,7 +21,7 @@ public class CompanyController {
 
     @PostMapping("/company")
     public ResponseEntity<MessageResponseDto> createCompany(
-            //test 2
+            //test 3
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestBody CreateCompanyRequestDto requestDto
     ) {
