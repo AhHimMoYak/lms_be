e505ebff81b7cdef39d8beaf6b8cbeb2911e4d2b
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-11-26 13:20:45 +0900

    mod : Curriculum get 메소드 생성

--- institution-service/src/main/java/com/example/ahimmoyakbackend/curriculum/controller/CurriculumController.java
+++ institution-service/src/main/java/com/example/ahimmoyakbackend/curriculum/controller/CurriculumController.java
@@ -3,6 +3,7 @@ package com.example.ahimmoyakbackend.curriculum.controller;
 
 import com.example.ahimmoyakbackend.course.dto.CurriculumCreateRequestDto;
 import com.example.ahimmoyakbackend.course.dto.CurriculumCreateResponseDto;
+import com.example.ahimmoyakbackend.curriculum.dto.CurriculumListByInstitutionResponseDto;
 import com.example.ahimmoyakbackend.curriculum.service.CurriculumService;
 import com.example.ahimmoyakbackend.global.dto.MessageResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/courses/{courseId}")
@@ -18,13 +21,21 @@ public class CurriculumController {
 
     private final CurriculumService curriculumService;
 
+    @GetMapping("/curriculums")
+    public ResponseEntity<List<CurriculumListByInstitutionResponseDto>> getCurriculumListByInstitution(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable Long courseId
+    ) {
+        return ResponseEntity.ok(curriculumService.get(userDetails, courseId));
+    }
+
     @PostMapping("/curriculums")
     public ResponseEntity<CurriculumCreateResponseDto> addCurriculum(
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable("courseId") long courseId,
             @RequestBody CurriculumCreateRequestDto requestDto
     ) {
-        return ResponseEntity.ok(curriculumService.add(userDetails, courseId, requestDto)) ;
+        return ResponseEntity.ok(curriculumService.add(userDetails, courseId, requestDto));
     }
 
     @PatchMapping("/curriculums/{curriculumId}")
