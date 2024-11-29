1dd7929d3b1607f0b7e6c0334b6730db69610cb5
Dowon <tnstjs1919@gmail.com>
2024-11-29 22:53:27 +0900

    test : test7

--- company-service/src/main/java/click/ahimmoyak/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/click/ahimmoyak/companyservice/CompanyServiceApplication.java
@@ -12,6 +12,6 @@ public class CompanyServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CompanyServiceApplication.class, args);
-        //test2
+        //test3
     }
 }
