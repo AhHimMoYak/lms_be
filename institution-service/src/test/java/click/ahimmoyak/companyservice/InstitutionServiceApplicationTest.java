fb2e4961bd9471d3883c945686ef281d378c7b58
Dowon <tnstjs1919@gmail.com>
2024-12-01 01:31:52 +0900

    mod : institution과 studnet github Actions test 오류 수정

--- institution-service/src/test/java/click/ahimmoyak/companyservice/InstitutionServiceApplicationTest.java
+++ institution-service/src/test/java/click/ahimmoyak/companyservice/InstitutionServiceApplicationTest.java
@@ -2,8 +2,10 @@ package click.ahimmoyak.companyservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = InstitutionServiceApplicationTest.class)
+@ActiveProfiles(profiles = "test")
 class InstitutionServiceApplicationTest {
 
     @Test
