0b19bd2b99debee2b78a71a27b40a2f14ebedfd7
Dowon <tnstjs1919@gmail.com>
2024-12-01 01:16:39 +0900

    mod : test 오류 수정

--- company-service/src/test/java/click/ahimmoyak/companyservice/CompanyServiceApplicationTests.java
+++ company-service/src/test/java/click/ahimmoyak/companyservice/CompanyServiceApplicationTests.java
@@ -2,8 +2,11 @@ package click.ahimmoyak.companyservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = CompanyServiceApplicationTests.class)
+@ActiveProfiles(profiles = "test")
 class CompanyServiceApplicationTests {
 
     @Test
