87651519ce4219a059576ba6f906a8c68f3ba1bc
Dowon <tnstjs1919@gmail.com>
2024-12-01 00:38:55 +0900

    mod : company github actions 수정

--- company-service/src/main/java/click/ahimmoyak/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/click/ahimmoyak/companyservice/CompanyServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CompanyServiceApplication {
 
     public static void main(String[] args) {
+        // test
         SpringApplication.run(CompanyServiceApplication.class, args);
     }
 }
