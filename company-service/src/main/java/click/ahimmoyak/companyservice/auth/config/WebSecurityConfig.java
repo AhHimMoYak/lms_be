a86a9e23079361a962ceb3b2a9912731a6636c04
Dowon <tnstjs1919@gmail.com>
2024-12-09 14:30:43 +0900

    mod : cors 설정 추가

--- company-service/src/main/java/click/ahimmoyak/companyservice/auth/config/WebSecurityConfig.java
+++ company-service/src/main/java/click/ahimmoyak/companyservice/auth/config/WebSecurityConfig.java
@@ -1,13 +1,14 @@
 package click.ahimmoyak.companyservice.auth.config;
 
-import click.ahimmoyak.companyservice.auth.repository.UserRepository;
+
 import click.ahimmoyak.companyservice.auth.config.handler.CustomAccessDeniedHandler;
 import click.ahimmoyak.companyservice.auth.jwt.JwtAuthFilter;
 import click.ahimmoyak.companyservice.auth.jwt.JwtTokenProvider;
+import click.ahimmoyak.companyservice.auth.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,11 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -37,52 +38,58 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
-
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .headers(headers -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(
                         XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
                 )))
-                .csrf(csrf -> csrf
+                .csrf((csrf) -> csrf
                         .csrfTokenRequestHandler(requestHandler)
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .ignoringRequestMatchers("/h2-console/**", "/api/v1/**")
+                        .ignoringRequestMatchers("/h2-console/**", "/v1/**")
                 )
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Arrays.asList(
+                                "http://localhost:5173",
+                                "http://localhost:5174",
+                                "https://local.ahimmoyak.click",
+                                "https://institution.local.ahimmoyak.click",
+                                "https://company.local.ahimmoyak.click",
+                                "https://lms.local.ahimmoyak.click",
+                                "https://ahimmoyak.click",
+                                "https://institution.ahimmoyak.click",
+                                "https://company.ahimmoyak.click",
+                                "https://lms.ahimmoyak.click"
+                        ));
+//                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.addExposedHeader("Authorization");
+                        config.addExposedHeader("refresh");
+                        config.setMaxAge(3600L);
+
+                        return config;
+                    }
+                }))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("api/v1/manager/test").hasRole("MANAGER")
-                        .anyRequest().permitAll()
+//                        .requestMatchers("api/v1/manager/test").hasRole("MANAGER")
+                                .anyRequest().permitAll()
                 )
                 .exceptionHandling(exceptionHandling ->
-                        exceptionHandling.accessDeniedHandler(new CustomAccessDeniedHandler())
+                                exceptionHandling
+                                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+//                                .authenticationEntryPoint(customAuthenticationEntryPoint)
                 )
-                .addFilterAfter(new JwtAuthFilter(jwtTokenProvider, userRepository), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(new CsrfCookieFilter(), CsrfFilter.class)
+                .addFilterAfter(new JwtAuthFilter(jwtTokenProvider, userRepository), UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
-    }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:5174",
-                "https://local.ahimmoyak.click",
-                "https://institution.local.ahimmoyak.click",
-                "https://company.local.ahimmoyak.click",
-                "https://lms.local.ahimmoyak.click"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowCredentials(true); // 인증 정보 포함
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.addExposedHeader("Authorization");
-        configuration.addExposedHeader("refresh");
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
